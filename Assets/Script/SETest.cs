36dd6bed8bfdead5a7677e0f6013c98ab8c5eca7
A601-20 <tnk.dat.class@gmail.com>
2021-07-10 16:01:47 +0900

    音を鳴らす　失敗

---
+++ Assets/Script/SETest.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SETest : MonoBehaviour
+{
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            TinyAudio.PlaySE(TinyAudio.SE.Menu_ugoki);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            TinyAudio.PlaySE(TinyAudio.SE.Menu_UP);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            TinyAudio.PlaySE(TinyAudio.SE.Kettei);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            TinyAudio.PlaySE(TinyAudio.SE.Cancel);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha5))
+        {
+            TinyAudio.PlaySE(TinyAudio.SE.Attack_niku);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha6))
+        {
+            TinyAudio.PlaySE(TinyAudio.SE.Attack_tomato);
+        }
+    }
+}
