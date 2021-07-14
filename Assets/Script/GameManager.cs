2b1f4a975fb7aa2fde610f8b3b439797e75811e4
A601-20 <tnk.dat.class@gmail.com>
2021-07-14 14:04:54 +0900

    Game切り替え

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -1,6 +1,9 @@
-﻿using System.Collections;
+﻿#define DEBUG_KEY
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -13,6 +16,15 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+#if DEBUG_KEY
+        if(Input.GetKeyDown(KeyCode.O))
+        {
+            SceneManager.LoadScene("Gameover");
+        }
+        else if(Input.GetKeyDown(KeyCode.C))
+        {
+            SceneManager.LoadScene("Clear");
+        }
+#endif
     }
 }
